db3db4e3471323c15b40baa46607c0d4b2535097
SleiYah <shy300602@gmail.com>
2025-03-09 02:39:05 +0200

    did user and userskele

---
+++ FAQ-Server/models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+class User {
+    private $conn;
+    
+    public function __construct($conn) {
+        $this->conn = $conn;
+    }
+    
+    public function create($userData) {
+        $user = new UserSkeleton();
+        $user->setFirstName($userData['first_name']);
+        $user->setLastName($userData['last_name']);
+        $user->setEmail($userData['email']);
+        $user->setPassword($userData['password']); 
+
+        $query = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("ssss", $user->getFirstName(), $user->getLastName(), $user->getEmail(), $user->getPassword());
+        $stmt->execute();
+        
+        return true;
+    }
+    
+    public function read($email) {
+        $query = "SELECT * FROM users WHERE email = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        return $result->fetch_assoc();
+    }
+    public function update($userData) {
+        $user = new UserSkeleton();
+        $user->getId($userData['user_id']);
+        $user->setFirstName($userData['first_name']);
+        $user->setLastName($userData['last_name']);
+        $user->setEmail($userData['email']);
+        
+        $query = "UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE user_id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("sssi", $user->getFirstName(), $user->getLastName(), $user->getEmail(), $user->getId());
+        $stmt->execute();
+        
+        return true;
+    }
+    
+    public function delete($userData) {
+        $user = new UserSkeleton();
+        $user->getId($userData['user_id']);
+        
+        $query = "DELETE FROM users WHERE user_id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bind_param("i", $user->getId());
+        $stmt->execute();
+        
+        return true;
+    }
+}
+?>
---
+++ FAQ-Server/models/UserSkeleton.php
@@ -0,0 +1,22 @@
+<?php
+class UserSkeleton {
+    protected $id;
+    protected $first_name;
+    protected $last_name;
+    protected $email;
+    protected $password;
+
+    public function setFirstName($first_name) { $this->first_name = $first_name; }
+    public function setLastName($last_name) { $this->last_name = $last_name; }
+
+    public function setEmail($email) { $this->email = $email; }
+    public function setPassword($password) { $this->password = password_hash($password, MHASH_SHA256); }
+
+    public function getId() { return $this->id; }
+    public function getFirstName() { return $this->first_name; }
+    public function getLastName() { return $this->last_name; }
+
+    public function getEmail() { return $this->email; }
+    public function getPassword() { return $this->password; }
+}
+?>
