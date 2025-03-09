38c2d37ee2d091ed1ef3fcec0b6f37d1b7fb03ab
SleiYah <shy300602@gmail.com>
2025-03-09 12:25:43 +0200

    pushing p

--- FAQ-Server/models/UserSkeleton.php
+++ FAQ-Server/models/UserSkeleton.php
@@ -11,7 +11,7 @@ class UserSkeleton {
     public function setLastName($last_name) { $this->last_name = $last_name; }
 
     public function setEmail($email) { $this->email = $email; }
-    public function setPassword($password) { $this->password = password_hash($password, MHASH_SHA256); }
+    public function setPassword($password) { $this->password = hash('sha256', $password); }
 
     public function getId() { return $this->id; }
     public function getFirstName() { return $this->first_name; }
