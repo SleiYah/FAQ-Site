f3a379a6024af20a87fc4596b22fb4f0b62a8ab9
SleiYah <shy300602@gmail.com>
2025-03-09 12:06:35 +0200

    pushing p

--- FAQ-Server/database/migrations/Migrate.php
+++ FAQ-Server/database/migrations/Migrate.php
@@ -2,8 +2,8 @@
 
 include(__DIR__ ."../../connection/conn.php");
 
-include(__DIR__ . "./CreateUser.php");
-include(__DIR__ . "./CreateFAQ.php");
+include(__DIR__ . "CreateUser.php");
+include(__DIR__ . "CreateFAQ.php");
 
 CreateUsersTable::users($conn);
 CreateFaqsTable::faqs($conn);
