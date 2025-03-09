9c3ab5aa90b158e337829e46bbcb7d1ad17ee468
SleiYah <shy300602@gmail.com>
2025-03-09 02:36:58 +0200

    conn

---
+++ FAQ-Server/connection/conn.php
@@ -0,0 +1,16 @@
+<?php 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS, POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+   
+    $conn = new mysqli("localhost", "root", "", "faq");
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+?>
